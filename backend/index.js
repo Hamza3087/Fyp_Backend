9af3a2b9d74bcc33c8626831f66eb69fdbb562d3
hamza_tariq <hamzagondal921@gmail.com>
2024-12-09 23:38:03 +0500

    first commit

---
+++ backend/index.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const cors = require('cors');
+
+const app = express();
+const port = 5000;
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Connect to MongoDB
+mongoose.connect('mongodb+srv://Bilalkhan:Pakistan@cluster1.moct8fi.mongodb.net/EmpoweredAI', {
+    
+})
+.then(() => console.log('MongoDB Connected'))
+.catch(err => console.log(err));
+
+// User Schema
+const userSchema = new mongoose.Schema({
+    username: { type: String, required: true },
+    password: { type: String, required: true },
+});
+
+const User = mongoose.model('User', userSchema);
+
+// Register route
+app.post('/register', async (req, res) => {
+    const { username, password } = req.body;
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({ username, password: hashedPassword });
+    try {
+        await newUser.save();
+        res.status(201).send('User registered');
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
+});
+
+// Login route
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+
+    if (!user) {
+        return res.status(400).send('User not found');
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+        return res.status(400).send('Invalid credentials');
+    }
+
+    const token = jwt.sign({ userId: user._id }, 'secretkey', { expiresIn: '1h' });
+    res.json({ token });
+});
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
